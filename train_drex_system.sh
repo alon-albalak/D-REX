4632b63e2bf5ed3d18e6444d35e8f850868abd33
Alon Albalak <alonalbalak@gmail.com>
2021-06-03 07:51:38 -0700

    use final models

--- train_drex_system.sh
+++ train_drex_system.sh
@@ -17,8 +17,8 @@ CUDA_VISIBLE_DEVICES=$GPU python3 train_relation_extraction_model.py \
     --relation_extraction_pretraining \
     > $MODEL_PATH/train_outputs.log
 
-BEST_INITIAL_RANKER=$(ls $MODEL_PATH/F1* -d | sort -r | head -n 1)
-echo "Found best initial ranker ${BEST_INITIAL_RANKER}"
+INITIAL_RANKER=$(find $MODEL_PATH -name final*)
+echo "Found final initial ranker ${INITIAL_RANKER}"
 
 # pre-train explanation policy
 MODEL_PATH=explanation_policy
@@ -34,18 +34,18 @@ CUDA_VISIBLE_DEVICES=$GPU python3 train_explanation_policy.py \
     --explanation_policy_pretraining \
     > $MODEL_PATH/train_outputs.log
 
-BEST_EXPL_POLICY=$(find $MODEL_PATH -name final*)
-echo "Found best explanation policy ${BEST_EXPL_POLICY}"
+EXPL_POLICY=$(find $MODEL_PATH -name final*)
+echo "Found final explanation policy ${EXPL_POLICY}"
 
 # predict explanations for all data partitions using best explanation policy
 CUDA_VISIBLE_DEVICES=$GPU python3 test_explanation_policy.py \
     --data_split=train \
     --model_class=explanation_policy_roberta \
-    --model_name_or_path=$BEST_EXPL_POLICY \
+    --model_name_or_path=$EXPL_POLICY \
     --base_model=roberta-base \
     --keep_case \
     --fp16 \
-    --output_dir=$BEST_EXPL_POLICY \
+    --output_dir=$EXPL_POLICY \
     --explanation_policy_pretraining \
     --predict_trigger_for_unlabelled \
     --gpu_batch_size=30
@@ -53,11 +53,11 @@ CUDA_VISIBLE_DEVICES=$GPU python3 test_explanation_policy.py \
 CUDA_VISIBLE_DEVICES=$GPU python3 test_explanation_policy.py \
     --data_split=dev \
     --model_class=explanation_policy_roberta \
-    --model_name_or_path=$BEST_EXPL_POLICY \
+    --model_name_or_path=$EXPL_POLICY \
     --base_model=roberta-base \
     --keep_case \
     --fp16 \
-    --output_dir=$BEST_EXPL_POLICY \
+    --output_dir=$EXPL_POLICY \
     --explanation_policy_pretraining \
     --predict_trigger_for_unlabelled \
     --gpu_batch_size=30
@@ -65,11 +65,11 @@ CUDA_VISIBLE_DEVICES=$GPU python3 test_explanation_policy.py \
 CUDA_VISIBLE_DEVICES=$GPU python3 test_explanation_policy.py \
     --data_split=test \
     --model_class=explanation_policy_roberta \
-    --model_name_or_path=$BEST_EXPL_POLICY \
+    --model_name_or_path=$EXPL_POLICY \
     --base_model=roberta-base \
     --keep_case \
     --fp16 \
-    --output_dir=$BEST_EXPL_POLICY \
+    --output_dir=$EXPL_POLICY \
     --explanation_policy_pretraining \
     --predict_trigger_for_unlabelled \
     --gpu_batch_size=30
@@ -86,18 +86,18 @@ CUDA_VISIBLE_DEVICES=$GPU python3 train_relation_extraction_model.py \
     --output_dir=$MODEL_PATH \
     --relation_extraction_conditioned_on_explanations \
     --use_predicted_explanations \
-    --predicted_explanation_path=$BEST_EXPL_POLICY \
+    --predicted_explanation_path=$EXPL_POLICY \
     > $MODEL_PATH/train_outputs.log
 
-BEST_RERANKER=$(ls $MODEL_PATH/F1* -d | sort -r | head -n 1)
-echo "Found best reranker ${BEST_RERANKER}"
+RERANKER=$(find $MODEL_PATH -name final*)
+echo "Found final reranker ${RERANKER}"
 
 MODEL_PATH=D_REX_system
 mkdir $MODEL_PATH
 CUDA_VISIBLE_DEVICES=$GPU python3 train_drex.py \
-    --relation_extraction_ranker_path=$BEST_INITIAL_RANKER \
-    --explanation_policy_path=$BEST_EXPL_POLICY \
-    --relation_extraction_reranker_path=$BEST_RERANKER \
+    --relation_extraction_ranker_path=$INITIAL_RANKER \
+    --explanation_policy_path=$EXPL_POLICY \
+    --relation_extraction_reranker_path=$RERANKER \
     --effective_batch_size=30 \
     --gpu_batch_size=1 \
     --fp16 \
