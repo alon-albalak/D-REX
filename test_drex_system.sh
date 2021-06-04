6c4b7a2a3aeeaabbad5c84774002bced6d4446c0
Alon Albalak <alonalbalak@gmail.com>
2021-06-04 08:50:55 -0700

    add test script

--- test_drex_system.sh
+++ test_drex_system.sh
@@ -1,10 +1,10 @@
 GPU=$1
 
-BEST_INITIAL_RANKER=$(ls initial_ranking_model/F1* -d | sort -r | head -n 1)
-echo "Found best initial ranker ${BEST_INITIAL_RANKER}"
-DREX_EXPLANATION_POLICY=$(find D_REX/ -name final.*expl_model)
+INITIAL_RANKER=$(find initial_ranking_model/ -name final*)
+echo "Found final initial ranker ${INITIAL_RANKER}"
+DREX_EXPLANATION_POLICY=$(ls D_REX_system/F1*expl_model -d | sort -r | head -n 1)
 echo "Found best explanation policy ${DREX_EXPLANATION_POLICY}"
-DREX_RERANKER=$(find D_REX/ -name final.*reranker_model)
+DREX_RERANKER=$(ls D_REX_system/F1*reranker_model -d | sort -r | head -n 1)
 echo "Found best reranker ${DREX_RERANKER}"
 
 THRESHOLD1=$(echo $DREX_RERANKER | grep -o "T1.....")
@@ -13,7 +13,7 @@ THRESHOLD2=$(echo $DREX_RERANKER | grep -o "T2.....")
 THRESHOLD2=${THRESHOLD2: -2}
 
 CUDA_VISIBLE_DEVICES=$GPU python3 test_drex.py \
-    --relation_extraction_ranker_path=$BEST_INITIAL_RANKER \
+    --relation_extraction_ranker_path=$INITIAL_RANKER \
     --explanation_policy_path=$DREX_EXPLANATION_POLICY \
     --relation_extraction_reranker_path=$DREX_RERANKER \
     --threshold1=$THRESHOLD1 \
